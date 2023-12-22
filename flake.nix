c18e676934884181ad8687e069d28db3c7d1cdfb
chayleaf <chayleaf-git@pavluk.org>
2023-12-22 20:34:32 +0700

    flake: fix build

--- flake.nix
+++ flake.nix
@@ -13,8 +13,9 @@
     inherit (nixpkgs) lib;
     mkPkgs = pkgs: let
       craneLib = crane.mkLib pkgs;
-    in {
-      default = craneLib.buildPackage {
+    in rec {
+      default = coop-ofd;
+      coop-ofd = craneLib.buildPackage {
         pname = "coop-ofd";
         version = "0.1.0";
         src = nixpkgs.lib.cleanSourceWith {
@@ -25,51 +26,59 @@
         };
         nativeBuildInputs = with pkgs; [ pkg-config rustPlatform.bindgenHook ];
         buildInputs = with pkgs; [ tesseract leptonica ];
+        doCheck = false;
       };
     };
   in {
     devShells = lib.genAttrs ["x86_64-linux" "aarch64-linux"] (system: let
       pkgs = import nixpkgs { inherit system; };
-    in {
-      default = import ./shell.nix { inherit pkgs; };
+    in rec {
+      default = coop-ofd;
+      coop-ofd = import ./shell.nix { inherit pkgs; };
     });
-    nixosModules.default = { lib, config, pkgs, ... }: let
-      cfg = config.services.coop-ofd;
-      format = pkgs.formats.json { };
-      cfgFile = format.generate "config.json" cfg.config;
-    in {
-      options.services.coop-ofd = {
-        enable = lib.mkEnableOption "coop-ofd";
-        package = lib.mkOption { type = lib.types.package; };
-        config = lib.mkOption {
-          type = lib.types.submodule {
-            freeformType = format.type;
-            options.data_path = lib.mkOption {
-              type = lib.types.path;
-              default = "/var/lib/coop-ofd";
+    nixosModules = rec {
+      default = coop-ofd;
+      coop-ofd = { lib, config, pkgs, ... }: let
+        cfg = config.services.coop-ofd;
+        format = pkgs.formats.json { };
+        cfgFile = format.generate "config.json" cfg.config;
+      in {
+        options.services.coop-ofd = {
+          enable = lib.mkEnableOption "coop-ofd";
+          package = lib.mkOption { type = lib.types.package; };
+          config = lib.mkOption {
+            type = lib.types.submodule {
+              freeformType = format.type;
+              options.data_path = lib.mkOption {
+                type = lib.types.path;
+                default = "/var/lib/coop-ofd";
+              };
+            };
+          };
+        };
+        config = lib.mkIf cfg.enable {
+          services.coop-ofd.package = lib.mkOptionDefault (mkPkgs pkgs).default;
+          systemd.services.coop-ofd = {
+            description = "coop-ofd";
+            after = [ "network.target" ];
+            wantedBy = [ "multi-user.target" ];
+            environment.CONFIG_FILE = cfgFile;
+            serviceConfig = {
+              DynamicUser = true;
+              ExecStart = "${cfg.package}/bin/coop-ofd";
+              Restart = "on-failure";
+              RestartSec = "10s";
+              StateDirectory = "coop-ofd";
+              WorkingDirectory = "/var/lib/coop-ofd";
             };
           };
         };
       };
-      config = lib.mkIf cfg.enable {
-        services.coop-ofd.package = lib.mkOptionDefault (mkPkgs pkgs).default;
-        systemd.services.coop-ofd = {
-          description = "coop-ofd";
-          after = [ "network.target" ];
-          wantedBy = [ "multi-user.target" ];
-          environment.CONFIG_FILE = cfgFile;
-          serviceConfig = {
-            DynamicUser = true;
-            ExecStart = "${cfg.package}/bin/coop-ofd";
-            Restart = "on-failure";
-            RestartSec = "10s";
-            StateDirectory = "coop-ofd";
-            WorkingDirectory = "/var/lib/coop-ofd";
-          };
-        };
-      };
     };
-    overlays.default = final: prev: mkPkgs final;
+    overlays = rec {
+      default = coop-ofd;
+      coop-ofd = final: prev: mkPkgs final;
+    };
     packages = lib.genAttrs ["x86_64-linux" "aarch64-linux"] (system: mkPkgs (import nixpkgs { inherit system; }));
   };
 }
